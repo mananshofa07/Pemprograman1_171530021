bd9f524e8c4d551a9e1f99f9c7875c681fdd5da3
Muhammad Manan Shofa <36910674+mananshofa07@users.noreply.github.com>
2018-05-01 21:09:29 -0700

    Method GET and POST

---
+++ praktikum2.php
@@ -0,0 +1,9 @@
+<html>
+<head><title>Pengolahan Form 2</title></HEAD>
+<body>
+    <fOrm action="proses02.php" method="POST" name="input">
+    Nama anda : <input type="text" name="nama"><br>
+    <input type="submit" name="input" value="input">
+    </form>
+</body>
+</html>
